5ca4758ad5ba7934ff23dd20a8015215ea9b919d
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-06-22 20:46:31 +0530

    stripe payment integrated

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const authRoute = require("./routes/Auth");
 const productRoute = require("./routes/ProductRoute");
 const cartRoute = require("./routes/CartRoute");
 const orderRoute = require("./routes/OrderRoute");
-const cors = require('cors');
-
+const stripeRoute = require("./routes/Stripe");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -22,13 +23,15 @@ mongoose
   });
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/product", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/order", orderRoute);
-
+app.use("/api/checkout", stripeRoute);
 app.listen(port, () => {
   console.log(`Server is running in port ${port}`);
 });
--- routes/Stripe.js
+++ routes/Stripe.js
@@ -1,17 +1,80 @@
+// This is your test secret API key.
+const stripe = require("stripe")(
+  "sk_test_51NL9zRSCTLZQuHdTjrw0lxdqvTxPgxiqSOXI19KEpqQNfmd7ELvrlTZ83ZQz80CPYXjxTKG4gQTwDpNsaIrPFlXV00bKcnvvl8"
+);
 const router = require("express").Router();
+router.post("/payment", async (req, res) => {
+  const line_items = req.body.map((item) => {
+    return {
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: `${item.brand}${item.desc}-${item.color}`,
+          images: [item.img],
+          metadata: { id: item._id },
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    shipping_address_collection: {
+      allowed_countries: ["IN"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 0,
+            currency: "inr",
+          },
+          display_name: "Free shipping",
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 5,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 1500,
+            currency: "inr",
+          },
+          display_name: "Next day air",
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 1,
+            },
+          },
+        },
+      },
+    ],
+    phone_number_collection: {
+      enabled: true,
+    },
 
-const stripe = require("stripe")(process.env.STRIPE_KEY);
-router.post("/payment", (req, res) => {
-  stripe.charges.create({
-    source:req.body.tokenId,
-    amount:req.body.amount,
-    currency:"inr"
-  },(stripeErr,stripeRes)=>{
-    if(stripeErr)
-        res.status(500).json(stripeErr);
-    else
-        res.status(200).json(stripeRes);
-  })
+    line_items,
+    mode: "payment",
+    success_url: "http://localhost:5173/success",
+    cancel_url: "http://localhost:5173/cancel",
+  });
+  res.send({ url: session.url });
 });
 
 module.exports = router;
+// app.listen(4242, () => console.log('Running on port 4242'));
